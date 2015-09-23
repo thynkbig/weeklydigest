aed54abb3612b0d04c96f0ad677ce21aba47972a
thynkbig <thynkbig@yahoo.com>
2015-09-22 22:42:21 -0500

    Added Log In button to homepage

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 	<div id="homepage">
 	<div id="content">
 	
-		<?php homepage_list(); ?>
+	<div class="login">
+		<a href="<?php echo bloginfo('url') . "/wp-admin"; ?>"><p><b>Log In</b></p></a>
+	</div>
+	<br/><br/><br/>
+	<?php homepage_list(); ?>
 	
 	</div>
 	</div>
